8e17fbdd194c8eabca1d6ef1399539a5a4f9b2fb
NogPiss <gorduranogueirapissuto@gmail.com>
2025-03-08 16:02:35 -0300

    primeiro commit

---
+++ RpgApi/Controllers/PersonagensExemploController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using RpgApi.Models;
+namespace RpgApi.Controllers{
+    [ApiController]
+    [Route("[Controller]")]
+
+    public class PersonagensExemploController: ControllerBase{
+
+        private static List<Personagem> personagens = new List<Personagem>(){
+            
+        }
+    }
+}
---
+++ RpgApi/Models/Enuns/ClasseEnum.cs
@@ -0,0 +1,8 @@
+namespace RpgApi.Models.Enuns 
+{
+    public enum ClasseEnum{
+        Cavaleiro = 1,
+        Mago = 2,
+        Clerigo = 3
+    }
+}
