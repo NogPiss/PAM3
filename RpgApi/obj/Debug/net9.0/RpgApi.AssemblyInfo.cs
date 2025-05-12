867099e90be5e51b80649c234f07c437b2cc6df9
unknown <NogPiss>
2025-05-12 08:03:17 -0300

    exclusao da pasta .github

--- RpgApi/obj/Debug/net9.0/RpgApi.AssemblyInfo.cs
+++ RpgApi/obj/Debug/net9.0/RpgApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ee4cb1bd96c1907d2bf850c593df052c9b3b51b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57d2b4627e51bad4be10db2de7565091c9f75181")]
 [assembly: System.Reflection.AssemblyProductAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RpgApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
